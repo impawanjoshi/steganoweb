3857caafc77d15a3de7a459bfd54a050377fac80
impawanjoshi <joshipawan814@gmail.com>
2020-08-06 20:40:22 +0530

    added frontend

--- steganoweb/views.py
+++ steganoweb/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html')
+
+def encode(request):
+    return render(request,'encode.html')
+    
+def decode(request):
+    return render(request,'decode.html')
